84b963d5036848824fdd8569b7957b3a7fdf37bc
Theeruth Borisuth <pwnimatorz@hotmail.com>
2017-03-03 12:31:07 +0700

    Update docpractice.java

--- src/docpractice.java
+++ src/docpractice.java
@@ -7,39 +7,39 @@ public class docpractice {
 	
 	public class Coin {
 		/*
-		 * 
+		 * TODO Write what this attribute is
 		 */
 		int value ;
 		/*
-		 * 
+		 * TODO Write what this method can do
 		 */
 		public Coin ()
 		{
 			this.value = 1 ;
 		}
 		/*
-		 * 
+		 * TODO Write what this method can do
 		 */
 		public Coin(int value)
 		{
 			this.value = value ;
 		}
 		/*
-		 * 
+		 * TODO Write what this method can do
 		 */
 		public void setValue(int value)
 		{
 			this.value = value ;
 		}
 		/*
-		 * 
+		 * TODO Write what this method can do
 		 */
 		public int  getValue ()
 		{
 			return value ;
 		}
 		/*
-		 * 
+		 * TODO Write what this method can do
 		 */
 		public String toString()
 		{
